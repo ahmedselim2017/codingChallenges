95142f2382a96f3b8f5c379777749796ec088c72
ahmedselimuzum <ahmedselimuzum@gmail.com>
2019-02-16 23:47:07 +0300

    Finishing Fourier Series

---
+++ FourierSeries/p5/script.js
@@ -0,0 +1,60 @@
+//EuzuBillahimineşşeydanirracim Bismillahirrahmanirrahim
+
+let time = 0;
+let wave = [];
+
+let slider;
+
+function setup() {
+    createCanvas(600,400);
+    slider = createSlider(1, 50, 5);
+
+}
+
+function draw() {
+    background(0);
+    translate(150, 200);
+
+    let x = 0;
+    let y = 0;
+
+    for(let i = 0; i < slider.value(); i++){
+        let prevX = x;
+        let prevY = y;
+
+        let n = i * 2 + 1;
+        let radius = 75 * ( 4 / (n * PI));
+
+        x += radius * cos(n * time);
+        y += radius * sin(n * time);
+
+        stroke(255, 100);
+        noFill();
+        ellipse(prevX, prevY, radius * 2);
+
+        stroke(255);
+        line(prevX,prevY, x,y);
+    }
+    wave.unshift(y);
+
+    translate(200, 0);
+    line(x - 200, y, 0, wave[0]);
+
+    beginShape();
+    noFill();
+    for (let i = 0; i < wave.length; i++) {
+        vertex(i, wave[i]);
+        }
+
+
+
+    endShape();
+
+    time += 0.05;
+
+    if(wave.lenght > 250){
+        wave.pop();
+    }
+
+
+ }
